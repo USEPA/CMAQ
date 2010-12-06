615999bbadfc5086fdab9f11aedee0dd2f530fb8
yoj <yoj>
2010-12-06 14:38:19 +0000

    modular aerosol process redesign; cosmetic changes

--- cloud/cloud_acm_ae5/getalpha.f
+++ cloud/cloud_acm_ae5/getalpha.f
@@ -18,21 +18,21 @@ C   permissions subject to the above restrictions.                     *
 C***********************************************************************
 
 C RCS file, release, date & time of last delta, author, state, [and locker]
-C $Header: /project/yoj/arc/CCTM/src/cloud/cloud_acm_ae5/Attic/getalpha.f,v 1.1 2010/07/20 11:56:47 yoj Exp $
+C $Header: /project/yoj/arc/CCTM/src/cloud/cloud_acm_ae5/Attic/getalpha.f,v 1.2 2010/12/06 14:38:19 yoj Exp $
 
 C what(1) key, module and SID; SCCS file; date and time of last delta:
 C %W% %P% %G% %U%
 
+C:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
       SUBROUTINE GETALPHA ( NUMI, MASSI, SURFI,
      &                      LWC, T, P, RHOAIR, ALFA0, ALFA2, ALFA3 )
 
 C-----------------------------------------------------------------------
-C
-C  DESCRIPTION:
+C  Description:
 C    Calculate the in-cloud scavenging coefficients for number (alfa0),
 C    surface area (alfa2) and mass (alfa3). This code combines the RPM
 C    codes getcld.f, and pandkcd.f
-C
+ 
 C  Revision History:
 C      Date       Who                      What
 C    -------- -------------------------  -------------------------------
@@ -54,131 +54,112 @@ C                                           code that are unnecessary in this
 C                                           implementation
 C-----------------------------------------------------------------------
 
+      USE AERO_DATA
+
       IMPLICIT NONE
 
-C...........PARAMETERS and their descriptions:
+C...........Parameters:
 
-      REAL          SIGC                ! geometric standard deviation for cloud droplets
-      PARAMETER   ( SIGC = 1.2 )
+      REAL, PARAMETER :: SIGC = 1.2     ! geometric standard deviation for cloud droplets
 
-      REAL          P0                  ! Standard pressure [ Pa ]
-      PARAMETER   ( P0 = 101325.0 )
+      REAL, PARAMETER :: P0 = 101325.0  ! Standard pressure [ Pa ]
 
-      REAL          T0                  !
-      PARAMETER   ( T0 = 288.0 )        ! [ K ]
+      REAL, PARAMETER :: T0 = 288.0
 
-      REAL          PI                  !
-      PARAMETER   ( PI = 3.14159265 )
+      REAL, PARAMETER :: PI = 3.14159265
 
+      REAL, PARAMETER :: ONE3 = 1.0 / 3.0
 
-      REAL          ONE3
-      PARAMETER   ( ONE3 = 1.0 / 3.0 )
+      REAL, PARAMETER :: TWO3 = 2.0 / 3.0
 
-      REAL          TWO3
-      PARAMETER   ( TWO3 = 2.0 / 3.0 )
+      REAL, PARAMETER :: KBOLTZ = 1.38066E-23 ! [ J/K ]
 
-      REAL          KBOLTZ              !
-      PARAMETER   ( KBOLTZ = 1.38066E-23 ) ! [ J/K ]
+      REAL, PARAMETER :: RHOPART = 1.0E3   ! particle density [ kg/ m**3 ]
 
-      REAL          RHOPART             ! particle density
-      PARAMETER   ( RHOPART = 1.0E3 )   ! [ kg/ m**3 ]
+!sjr  REAL, PARAMETER :: AERCONST = 6.0E-9 / ( RHOPART * PI / 6.0 )
+      REAL, PARAMETER :: AERCONST = 1.0E-9 / ( RHOPART * PI / 6.0 )
 
-      REAL          AERCONST            !
-csjr      PARAMETER   ( AERCONST = 6.0E-9 / ( RHOPART *  PI / 6.0 ) )
-      PARAMETER   ( AERCONST = 1.0E-9 / ( RHOPART *  PI / 6.0 ) )
+      REAL, PARAMETER :: GRAV = 9.8        ! [ m/s**2 ]
 
-      REAL          GRAV                !
-      PARAMETER   ( GRAV = 9.8 )        ! [ m/s**2 ]
+      REAL, PARAMETER :: RHO_WATER = 1.0E3 ! density of water at 20 C & 1 ATM [ kg/m**3 ]
 
-      REAL          RHO_WATER           ! density of water at 20 C and 1 ATM
-      PARAMETER   ( RHO_WATER = 1.0E3 ) ! [ kg/m**3 ]
+C...........Arguments:
 
-C...........ARGUMENTS and their descriptions
+      REAL          NUMI       ! total number concentration for the Aitken mode [ #/m**3 ]
+      REAL          MASSI      ! total mass concentration in AItken mode [ ug/m**3 ]
+      REAL          SURFI      ! total surface area in Aitken mode [ m**2 / m**3 ]
+      REAL          LWC        ! cloud liquid water content [ kg/m**3 ]
+      REAL          T          ! average cloud temperature [ K ]
+      REAL          P          ! average cloud pressure [ Pa ]
+      REAL          RHOAIR     ! average cloud density [ kg/m**3 ]
+      REAL          ALFA0      ! scavenging coefficient for number [ 1/s ]
+      REAL          ALFA2      ! scavenging coefficient for surface area [ 1/s ]
+      REAL          ALFA3      ! scavenging coefficient for mass [ 1/s ]
 
-      REAL          NUMI                ! total number concentration for the Aitken mode [ #/m**3 ]
-      REAL          MASSI               ! total mass concentration in AItken mode  [ ug/m**3 ]
-      REAL          SURFI               ! total surface area in Aitken mode [ m**2 / m**3 ]
-      REAL          LWC                 ! cloud liquid water content [ kg/m**3 ]
-      REAL          T                   ! average cloud temperature [ K ]
-      REAL          P                   ! average cloud pressure [ Pa ]
-      REAL          RHOAIR              ! average cloud density [ kg/m**3 ]
-      REAL          ALFA0               ! scavenging coefficient for number [ 1/s ]
-      REAL          ALFA2               ! scavenging coefficient for surface area [ 1/s ]
-      REAL          ALFA3               ! scavenging coefficient for mass   [ 1/s ]
+C...........Local Variables (scalars):
 
-C...........LOCAL VARIABLES (scalars) and their descriptions:
-
-      REAL          DGNI                ! geometric mean diameter for AItken mode [ m ]
-      REAL          L2SGI               ! square of the log of the
-                                        ! Aitken mode geometric standard deviation
-c      REAL          XXFN                ! fraction for computing L2SGI
+      REAL          DGNI       ! geometric mean diameter for AItken mode [ m ]
+      REAL          L2SGI      ! square of the log of the
+                               ! Aitken mode geometric standard deviation
+!     REAL          XXFN       ! fraction for computing L2SGI
 
 c *** ranges for acceptable values of LOG( sigma_g).
 
-      REAL          MINL2SG             ! minimum value of L2SG
-      PARAMETER   ( MINL2SG = 2.380480e-3 )  ! minimum sigma_g = 1.05
+      REAL, SAVE   :: MINL2SG  ! minimum value of L2SG
+      REAL, SAVE   :: MAXL2SG  ! maximum value of L2SG
+      REAL, SAVE   :: DEFL2SG  ! default value of L2SG if no surface area
 
-      REAL          MAXL2SG             ! maximum value of L2SG
-      PARAMETER   ( MAXL2SG = 8.395887e-1 )  ! maximum sigma_g = 2.5
+      REAL          NC         ! total cloud droplet number concentration  [ #/m**3 ]
+      REAL          DGC        ! geometric mean diameter for cloud droplet [ m ]
+      REAL          ESG4       ! exp(0.5 * L2SGI )
+      REAL          ESG16      ! exp(2.0 * L2SGI )
+      REAL          ESG36      ! exp(4.5 * L2SGI )
+      REAL          DG2        ! square of geometic mean diameter
+      REAL          AMM2       ! -2nd moment of the Aitken mode distribution
+      REAL          AMM1       ! -1st moment of the Aitken mode distribution
+      REAL          AM1        ! 1st moment of the Aitken mode distribution
+      REAL          AM0        ! alternative name for Aitken mode number  [ #/m**3]
+      REAL          AM2        ! 2nd moment of the Aitken mode distribution
+      REAL          AM3        ! 3rd moment of the Aitken mode distribution
+      REAL          M0C        ! moments of the cloud droplet distribution
+      REAL          M1C        ! moments of the cloud droplet distribution
+      REAL          M2C        ! moments of the cloud droplet distribution
 
-      REAL          DEFL2SG             ! default value of L2SG if no surface area
-      PARAMETER   ( DEFL2SG = 2.815663e-1 )  ! default sigma_g = 1.7
+      REAL          LSGC, LSGC2  ! log and square of log of SIGC
+      REAL, SAVE :: ESGC4      !
+      REAL, SAVE :: ESGC16     !
 
-      REAL          NC                  ! total cloud droplet number concentration  [ #/m**3 ]
-      REAL          DGC                 ! geometric mean diameter for cloud droplet [ m ]
-      REAL          ESG4                ! exp(0.5 * L2SGI )
-      REAL          ESG16               ! exp(2.0 * L2SGI )
-      REAL          ESG36               ! exp(4.5 * L2SGI )
-      REAL          DG2                 ! square of geometic mean diameter
-      REAL          AMM2                ! -2nd moment of the Aitken mode distribution
-      REAL          AMM1                ! -1st moment of the Aitken mode distribution
-      REAL          AM1                 ! 1st moment of the Aitken mode distribution
-      REAL          AM0                 ! alternative name for Aitken mode number  [ #/m**3]
-      REAL          AM2                 ! 2nd moment of the Aitken mode distribution
-      REAL          AM3                 ! 3rd moment of the Aitken mode distribution
-      REAL          M0C                 ! moments of the cloud droplet distribution
-      REAL          M1C                 ! moments of the cloud droplet distribution
-      REAL          M2C                 ! moments of the cloud droplet distribution
+      REAL          COEF       !
+      REAL          DIFF0      ! aerosol diffusivities
+      REAL          DIFF2      ! aerosol diffusivities
+      REAL          DIFF3      ! aerosol diffusivities
+      REAL          PE0        ! Peclet numbers
+      REAL          PE2        ! Peclet numers
+      REAL          PE3        ! Peclet numbers
+      REAL          COEF2      !
+      REAL          COEF3      !
+      REAL          COEF4      !
+      REAL          VSETTL     ! settling velocity for cloud drops
+      REAL          LAMDA      ! mean free path of air [ m ]
+      REAL          MU         ! dynamic viscosity [ kg/(m*s) ]
+      REAL          NU         ! kinematic viscosity [ m**2/s ]
 
-      REAL          LSGC, LSGC2         ! log and square of log of SIGC
-      REAL          ESGC4               !
-      SAVE          ESGC4
-      REAL          ESGC16              !
-      SAVE          ESGC16
+      REAL          CUBRT      ! cube root
+      REAL          XX         ! dummy arguement for cube root
 
-      REAL          COEF                !
-      REAL          DIFF0               ! aerosol diffusivities
-      REAL          DIFF2               ! aerosol diffusivities
-      REAL          DIFF3               ! aerosol diffusivities
-      REAL          PE0                 ! Peclet numbers
-      REAL          PE2                 ! Peclet numers
-      REAL          PE3                 ! Peclet numbers
-      REAL          COEF2               !
-      REAL          COEF3               !
-      REAL          COEF4               !
-      REAL          VSETTL              ! settling velocity for cloud drops
-      REAL          LAMDA               ! mean free path of air [ m ]
-      REAL          MU                  ! dynamic viscosity [ kg/(m*s) ]
-      REAL          NU                  ! kinematic viscosity [ m**2/s ]
+      REAL, SAVE :: CLCONST    !
 
-      REAL          CUBRT               ! cube root
-      REAL          XX                  ! dummy arguement for cube root
+      LOGICAL, SAVE :: FIRSTIME = .TRUE.   ! flag for first pass thru
 
-      REAL          CLCONST             !
-      SAVE          CLCONST             !
-
-      LOGICAL       FIRSTIME            ! flag for first pass thru
-      DATA          FIRSTIME / .TRUE. /
-      SAVE          FIRSTIME
-
-C...........STATEMENT FUNCTIONS:
+C...........Statement Functions:
 
       CUBRT( XX ) = EXP( ONE3 * LOG( XX ) )
 
-C*********************************************************************
+C-----------------------------------------------------------------------
 C     begin body of subroutine GETALPHA
 
       IF ( FIRSTIME ) THEN
+        FIRSTIME = .FALSE.
 
 C...compute special variables for moments
 C...  of cloud droplet distribution
@@ -190,18 +171,19 @@ C...  of cloud droplet distribution
 
 C...compute constant for obtaining dgc
 
-        CLCONST = 6.0 /
-     &     ( RHO_WATER * PI * EXP( 4.5 * LOG( SIGC ) ** 2 ) )
+        CLCONST = 6.0 / ( RHO_WATER * PI * EXP( 4.5 * LOG( SIGC ) ** 2 ) )
 
-        FIRSTIME = .FALSE.
+        MINL2SG = LOG( MIN_SIGMA_G ) ** 2.0   ! minimum sigma_g = 1.05 (2.380480e-3)
+        MAXL2SG = LOG( MAX_SIGMA_G ) ** 2.0   ! maximum sigma_g = 2.5  (8.395887e-1)
+        DEFL2SG = LOG( DEF_SIGMA_G ) ** 2.0   ! default sigma_g = 1.7  (2.815663e-1)
 
       END IF ! check on firstime
 
 C...check to make sure that there is aerosol mass and number before
 C... proceeding, if not set alphas to zero
 
-cccc FSB      IF ( ( NUMI .LE. 0.0 ) .OR. ( MASSI .LE. 0.0 ) .OR.
-cccc FSB    &     ( LWC  .LE. 0.0 ) ) THEN
+!cc FSB      IF ( ( NUMI .LE. 0.0 ) .OR. ( MASSI .LE. 0.0 ) .OR.
+!cc FSB    &     ( LWC  .LE. 0.0 ) ) THEN
 
 C FSB mass check changed to be 1.0e-6 [ug/m**3] - 08/23/2005
 C     to avoid AM3 becomming too small.  With this change
@@ -209,7 +191,7 @@ C     AM3 is no smaller than 1.0e-18
 
       IF ( ( NUMI .LE. 0.0 ) .OR. ( MASSI .LE. 1.0e-6 ) .OR.
      &     ( LWC  .LE. 0.0 ) ) THEN
-cccc     &     ( SURFI .LE. 0.0 ) .OR. ( LWC  .LE. 0.0 ) ) THEN
+!cc     &     ( SURFI .LE. 0.0 ) .OR. ( LWC  .LE. 0.0 ) ) THEN
         ALFA0 = 0.0
         ALFA2 = 0.0
         ALFA3 = 0.0
@@ -320,9 +302,9 @@ C...                             pp 380 - 384, 1978, D. Reidel
 
 C...get aerosol diffusivities
 
-cccc FSB these values are not used
-cccc      DIFF0 = COEF * ( AMM1 + 2.492 * LAMDA * AMM2 ) / AM0
-cccc      DIFF2 = COEF * ( AM1  + 2.492 * LAMDA * AM0  ) / AM2
+!cc FSB these values are not used
+!cc      DIFF0 = COEF * ( AMM1 + 2.492 * LAMDA * AMM2 ) / AM0
+!cc      DIFF2 = COEF * ( AM1  + 2.492 * LAMDA * AM0  ) / AM2
       DIFF3 = COEF * ( ( AM2  + 2.492 * LAMDA * AM1  ) / AM3 )
 
 C...set up coefficient coef2 for settling velocity of cloud drops
@@ -339,10 +321,10 @@ C...set up for Peclet numbers
 
 C...calculate Peclet numbers
 
-cccc FSB because these Peclet numbers are not used, I have commented 
-cccc     these lines of code - Binkowski 08/23/2005
-cccc      PE0 = COEF3 / DIFF0
-cccc      PE2 = COEF3 / DIFF2
+!cc FSB because these Peclet numbers are not used, I have commented 
+!cc     these lines of code - Binkowski 08/23/2005
+!cc      PE0 = COEF3 / DIFF0
+!cc      PE2 = COEF3 / DIFF2
       PE3 = COEF3 / DIFF3
 
 C...now get in-cloud scavenging coefficients
@@ -350,10 +332,10 @@ C...  including convective enhancement
 
       COEF4  = 2.0 * PI * M1C
 
-cccc FSB use a common value, that for 3rd moment.
-cccc
-cccc      ALFA0 = COEF4 * DIFF0 * ( 1.0 + 0.5 * CUBRT( PE0 ) )
-cccc      ALFA2 = COEF4 * DIFF2 * ( 1.0 + 0.5 * CUBRT( PE2 ) )
+!cc FSB use a common value, that for 3rd moment.
+!cc
+!cc      ALFA0 = COEF4 * DIFF0 * ( 1.0 + 0.5 * CUBRT( PE0 ) )
+!cc      ALFA2 = COEF4 * DIFF2 * ( 1.0 + 0.5 * CUBRT( PE2 ) )
       ALFA3 = COEF4 * DIFF3 * ( 1.0 + 0.5 * CUBRT( PE3 ) )
       ALFA0 = ALFA3
       ALFA2 = ALFA3
