228fb8df96cf296d52fdbb4d78542ff759bb356b
Golam Sarwar <sarwar.golam@epa.gov>
2017-10-12 13:24:19 -0400

    Removed the duplicate heterogeneous reaction of N2O5: <HET_N2O5

--- CCTM/src/MECHS/saprc07tic_ae6i_aq/RXNS_DATA_MODULE.F90
+++ CCTM/src/MECHS/saprc07tic_ae6i_aq/RXNS_DATA_MODULE.F90
@@ -6,7 +6,7 @@
 
 
 ! --------- Photochemical Mechanism Reactions, Rates, etc. DAT ---------
-! Source file: /home/sgq/util/MP/saprc07tic_ae6i_aq/mech.def
+! Source file: /home/gsarwar/util/MP/saprc07tic_ae6i_aq/mech.def
 ! for Mechanism Name: SAPRC07TIC_AE6I_AQ              
 
 ! This file is used to create mechanism data and functions
