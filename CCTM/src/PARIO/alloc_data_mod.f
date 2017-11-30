7846c28ba3e9b497c59512f153ac6a49459ee397
barronh <barronh@gmail.com>
2017-11-30 10:50:11 -0500

    Vertical Column Extraction
    This commit adds a working vertical column extraction via 3 environmental
    variables: DOVERTEXT, VERTEXTLAT and VERTEXTLON
    
    For example, the following environmental variables enable extraction at
    I,J ((30,30), (40,40), (50,50), (60,60)) for the current test case.
    
     setenv DOVERTEXT "Y"
     setenv VERTEXTLAT "32.8372 33.7499 34.6438 35.5179"
     setenv VERTEXTLON "-84.5684 -83.1034 -81.603 -80.0667"
    
    To-do: Add more error checking and disable WR_VEXT when DOVERTEXT is off

--- CCTM/src/PARIO/alloc_data_mod.f
+++ CCTM/src/PARIO/alloc_data_mod.f
@@ -33,6 +33,8 @@ C....................................................................
       MODULE ALLOC_DATA_MODULE
 
       REAL, ALLOCATABLE, SAVE :: WRITBUF( :,:,: )
+      REAL, ALLOCATABLE, SAVE :: WRITVBUF( :,: )
       REAL, ALLOCATABLE, SAVE :: RECVBUF( : )
+      REAL, ALLOCATABLE, SAVE :: RECVVBUF( : )
 
       END MODULE ALLOC_DATA_MODULE
