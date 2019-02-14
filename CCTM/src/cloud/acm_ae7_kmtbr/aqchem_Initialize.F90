76779f4673f05f5acb8fe7126e341a2e2d0c099e
Kathleen Fahey <fahey.kathleen@epa.gov>
2019-02-14 10:10:35 -0500

    Updated KMTBR integrator/tolerances and final cl redistribution after cloud processing to correct the periodic mass balance issues that were showing up for NHx and Cl in some cells during the hemispheric simulations.  On branch KMTBR-update 	modified:   CCTM/src/cloud/acm_ae7_kmtbr/aqchem_Initialize.F90 	modified:   CCTM/src/cloud/acm_ae7_kmtbr/aqchem_Integrator.F90 	modified:   CCTM/src/cloud/acm_ae7_kmtbr/aqchem_kmt.F90

--- CCTM/src/cloud/acm_ae7_kmtbr/aqchem_Initialize.F90
+++ CCTM/src/cloud/acm_ae7_kmtbr/aqchem_Initialize.F90
@@ -215,11 +215,15 @@ CONTAINS
       DT     = TAUCLD
       TEND = TSTART + DT
       RTOL = 1.0D-02
-      ATOL = 1.0D+02  ! was 1.d-02
+      ATOL = 1.0D-02
      
       RTOL( ind_L_H2O2 ) = 1.0d-3
       RTOL( ind_L_MGLY ) = 1.0d-3
       RTOL( ind_L_GLY )  = 1.0d-3
+      
+      RTOL( ind_L_NH4PLUS )  = 1.0d-3
+      RTOL( ind_G_HCL )  = 1.0d-3      
+      ATOL( ind_L_HOBR ) = 1.0d-8
 
       DDIAM = 1.6D-5 ! Droplet diameter (meters)
 
