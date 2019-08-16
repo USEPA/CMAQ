46aa7ed7068317ffd4c06c926d023cf65cb6e0e8
fsidi <fisidi@ncsu.edu>
2019-08-16 16:40:21 -0400

    modified:   ../aero/aero6/aero_driver.F 	modified:   ../cio/centralized_io_module.F 	modified:   ../cio/centralized_io_util_module.F 	modified:   ../cloud/acm_ae6/convcld_acm.F 	modified:   ../cloud/acm_ae6/rescld.F 	modified:   ../cloud/acm_ae6_mp/convcld_acm.F 	modified:   ../cloud/acm_ae6_mp/rescld.F 	modified:   ../cloud/acm_ae7_kmt2/convcld_acm.F 	modified:   ../cloud/acm_ae7_kmt2/rescld.F 	modified:   ../driver/driver.F 	modified:   ../emis/emis/PTMET.F 	modified:   ../hdiff/multiscale/rho_j.F 	modified:   ../phot/inline/concld_prop_acm.F 	modified:   twoway_aqprep.F90 	modified:   twoway_cgrid_aerosol_spc_map_module.F90 	modified:   twoway_data_module.F90 	modified:   twoway_feedback.F90 	modified:   twoway_init_env_vars.F90 	modified:   twoway_met_param_module.F90 	modified:   twoway_sd_time_series.F90 	modified:   ../util/util/RUNTIME_VARS.F

--- CCTM/src/twoway/twoway_init_env_vars.F90
+++ CCTM/src/twoway/twoway_init_env_vars.F90
@@ -24,50 +24,50 @@
 ! Name:     twoway_init_env_vars Prep
 ! Purpose:  process all twoway related environment variables
 ! Revised:  31 Jan 2019  Original version.  (D. Wong)
+!           01 Aug 2019  (D. Wong)
+!              -- removed usage of status in calling get_env
 !===============================================================================
 
           use twoway_data_module
           use sd_time_series_module, only : sd_scol, sd_ecol, sd_srow, sd_erow
           use get_env_module
 
-          integer :: status
+          call get_env (wrf_restart, "WRF_RSTFLAG", .false.)
 
-          call get_env (wrf_restart, "WRF_RSTFLAG", .false., status)
+          call get_env (sd_time_series, 'SD_TIME_SERIES', .false.)
 
-          call get_env (sd_time_series, 'SD_TIME_SERIES', .false., status)
+          call get_env (create_physical_file, 'CREATE_PHYSICAL_FILE', .false.)
 
-          call get_env (create_physical_file, 'CREATE_PHYSICAL_FILE', .false., status)
+          call get_env (wrf_cmaq_freq, 'WRF_CMAQ_FREQ', 1)
 
-          call get_env (wrf_cmaq_freq, 'WRF_CMAQ_FREQ', 1, status)
+          call get_env (run_cmaq_driver, 'RUN_CMAQ_DRIVER', .false.)
 
-          call get_env (run_cmaq_driver, 'RUN_CMAQ_DRIVER', .false., status)
+          call get_env (cmaq_wrf_feedback, 'CMAQ_WRF_FEEDBACK', .false.)
 
-          call get_env (cmaq_wrf_feedback, 'CMAQ_WRF_FEEDBACK', .false., status)
+          call get_env (indirect_effect, 'INDIRECT_EFFECT', .false.)
 
-          call get_env (indirect_effect, 'INDIRECT_EFFECT', .false., status)
+          call get_env (cmaq_sdate, 'CTM_STDATE', 0)
 
-          call get_env (cmaq_sdate, 'CTM_STDATE', 0, status)
+          call get_env (cmaq_stime, 'CTM_STTIME', 0)
 
-          call get_env (cmaq_stime, 'CTM_STTIME', 0, status)
+          call get_env (turn_on_pv, 'CTM_TURN_ON_PV', .false.)
 
-          call get_env (turn_on_pv, 'CTM_TURN_ON_PV', .false., status)
+          call get_env (wrf_lc_ref_lat, 'WRF_LC_REF_LAT', 0.0)
 
-          call get_env (wrf_lc_ref_lat, 'WRF_LC_REF_LAT', 0.0, status)
+          call get_env (delta_x, 'TWOWAY_DELTA_X', 5)
 
-          call get_env (delta_x, 'TWOWAY_DELTA_X', 5, status)
+          call get_env (delta_y, 'TWOWAY_DELTA_Y', 5)
 
-          call get_env (delta_y, 'TWOWAY_DELTA_Y', 5, status)
-
-          call get_env (file_time_step, 'FILE_TIME_STEP', 10000, status)
+          call get_env (file_time_step, 'FILE_TIME_STEP', 10000)
 
           if (sd_time_series) then
-             call get_env (sd_scol, 'SD_SCOL', 1, status)
-             call get_env (sd_ecol, 'SD_ECOL', 1, status)
-             call get_env (sd_srow, 'SD_SROW', 1, status)
-             call get_env (sd_erow, 'SD_EROW', 1, status)
+             call get_env (sd_scol, 'SD_SCOL', 1)
+             call get_env (sd_ecol, 'SD_ECOL', 1)
+             call get_env (sd_srow, 'SD_SROW', 1)
+             call get_env (sd_erow, 'SD_EROW', 1)
           end if
 
-          call get_env (griddesc_fname, 'GRIDDESC', ' ', status)
-          call get_env (grid_name_str, 'GRID_NAME', ' ', status)
+          call get_env (griddesc_fname, 'GRIDDESC', ' ')
+          call get_env (grid_name_str, 'GRID_NAME', ' ')
 
         end subroutine twoway_init_env_vars
--- CCTM/src/twoway/twoway_met_param_module.F90
+++ CCTM/src/twoway/twoway_met_param_module.F90
@@ -5,6 +5,10 @@
 !           July, 16 2013  David Wong -- corrected the unit for RA and RS
 !                                     -- added DLUSE in GRIDCRO2D
 !           Jan, 11 2016  David Wong -- added a new variable PV
+!           Mar 04, 2019  Gilliam and Wong -- added new metcro2d variables 
+!                         according to new PX implementation in WRFv4.1+
+!           Aug 01, 2019  Wong -- added two new metdot3d variables, UWIND and
+!                                 VWIND (wind component of the mass point)
 !===============================================================================
 
   module twoway_met_param_module
@@ -12,9 +16,9 @@
     INTEGER, PARAMETER :: max_nvars       = 1000
     INTEGER, PARAMETER :: n_gridcro2d_var = 7
     INTEGER, PARAMETER :: n_griddot2d_var = 1
-    INTEGER, PARAMETER :: n_metcro3d_var  = 15
+    INTEGER, PARAMETER :: n_metcro3d_var  = 17
     INTEGER, PARAMETER :: n_metdot3d_var  = 4
-    INTEGER, PARAMETER :: n_metcro2d_var  = 32
+    INTEGER, PARAMETER :: n_metcro2d_var  = 38
 
     CHARACTER (LEN = 16), PARAMETER :: gridcro2d_vlist(n_gridcro2d_var) = &
       (/ 'LAT             ', 'LON             ',            &
@@ -42,6 +46,7 @@
          'QS              ', 'QG              ',            &
          'PRES            ', 'DENS            ',            &
          'ZH              ', 'ZF              ',            &
+         'UWIND           ', 'VWIND           ',            &
          'PV              '                      /)
 
     CHARACTER (LEN = 16), PARAMETER :: metcro3d_units(n_metcro3d_var) = &
@@ -52,6 +57,7 @@
          'KG/KG           ', 'KG/KG           ',            &
          'Pa              ', 'KG/M**3         ',            &
          'M               ', 'M               ',            &
+         'M/S             ', 'M/S             ',            &
          'M^2*K/KG/S * E-6'                      /)
 
     CHARACTER (LEN = 16), PARAMETER :: metdot3d_vlist(n_metdot3d_var) =  &
@@ -78,7 +84,10 @@
          'SLTYP           ', 'Q2              ',            &
          'SEAICE          ', 'SOIM1           ',            &
          'SOIM2           ', 'SOIT1           ',            &
-         'SOIT2           ', 'LH              ' /)
+         'SOIT2           ', 'LH              ',            &
+         'WWLT_PX         ', 'WFC_PX          ',            &
+         'WSAT_PX         ', 'CLAY_PX         ',            &
+         'CSAND_PX        ', 'FMSAND_PX       ' /)
 
     CHARACTER (LEN = 16), PARAMETER :: metcro2d_units(n_metcro2d_var) = &
       (/ 'Pascal          ', 'M/S             ',            &
@@ -96,6 +105,9 @@
          '-               ', 'KG/KG           ',            &
          'FRACTION        ', 'M**3/M**3       ',            &
          'M**3/M**3       ', 'K               ',            &
-         'K               ', 'WATTS/M**2      ' /)
+         'K               ', 'WATTS/M**2      ',            &
+         'M**3/M**3       ', 'M**3/M**3       ',            &
+         'M**3/M**3       ', 'FRACTION        ',            &
+         'FRACTION        ', 'FRACTION        ' /)
 
-  end module twoway_met_param_module
+end module twoway_met_param_module
